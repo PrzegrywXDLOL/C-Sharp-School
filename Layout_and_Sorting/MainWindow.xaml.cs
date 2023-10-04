037f5d3023e49fd8a5d9ab243d99c2c2815f9dab
PrzegrywXDLOL <142092638+PrzegrywXDLOL@users.noreply.github.com>
2023-10-04 21:33:12 +0200

    Initial commit

--- Layout_and_Sorting/MainWindow.xaml.cs
+++ Layout_and_Sorting/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Layout
 {
@@ -20,21 +22,36 @@ namespace Layout
     /// </summary>
     public partial class MainWindow : Window
     {
-        Button[] buttons;
+        List<Button> buttons;
         Random random;
         int count;
         int[] tab;
+        DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
-            buttons = new Button[4];
-            random = new Random();
-            count = 0;
 
-            for(int i = 0; i < buttons.Length; i++)
+            Decide decide = new Decide();
+            decide.ShowDialog();
+
+            int.TryParse(decide.number.Text.ToString(), out int btn);
+
+            buttons = new List<Button>();
+            random = new Random();
+            timer = new DispatcherTimer();
+
+            CreateBtn(btn);
+        }
+
+        private void CreateBtn(int btn)
+        {
+            buttons.Clear();
+            uniform.Children.Clear();
+
+            for (int i = 0; i < btn; i++)
             {
-                buttons[i] = new Button();
-                buttons[i].Content = random.Next(10);
+                buttons.Add(new Button());
+                buttons[i].Content = random.Next(10).ToString();
                 buttons[i].FontSize = 50;
 
                 buttons[i].Click += leftClick;
@@ -42,72 +59,99 @@ namespace Layout
 
                 uniform.Children.Add(buttons[i]);
             }
+            progress.Value = SortPercent();
         }
 
         private void leftClick(object sender, RoutedEventArgs e)
         {
-            count = 0;
             var button = (Button)sender;
-            button.Content = random.Next(10);
+            button.Content = random.Next(10).ToString();
+            progress.Value = SortPercent();
         }
 
         private void rightClick(object sender, MouseButtonEventArgs e)
         {
-            count = 0;
             foreach (var item in buttons) 
             {
-                item.Content = random.Next(10);
+                item.Content = random.Next(10).ToString();
             }
+            progress.Value = SortPercent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string textTemp = string.Empty;
-            tab = new int[buttons.Length];
-            
-            for (int i = 0; i < buttons.Length; i++)
-                tab[i] = int.Parse(buttons[i].Content.ToString());
-            
-            while (!IsSort())
+            count = 0;
+            text.Text = string.Empty;
+
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (IsSort())
+            {
+                timer.Stop();
+                MessageBox.Show("Number of attempts: " + count);
+                Decide decide1 = new Decide();
+                decide1.ShowDialog();
+                int.TryParse(decide1.number.Text.ToString(), out int btn);
+                CreateBtn(btn);
+            }
+            else
             {
                 count++;
-                for(int i = 0; i <= buttons.Length; i++)
+                for (int i = 1; i < buttons.Count; i++)
                 {
-                    int a = random.Next(buttons.Length);
-                    int b = random.Next(buttons.Length);
-
-                    var temp = tab[a];
-                    tab[a] = tab[b];
-                    tab[b] = temp;
+                    if (int.Parse(buttons[i - 1].Content.ToString()) > int.Parse(buttons[i].Content.ToString()))
+                    {
+                        var temp = buttons[i - 1].Content.ToString();
+                        buttons[i - 1].Content = buttons[i].Content.ToString();
+                        buttons[i].Content = temp;
+                    } 
                 }
 
-                textTemp += count.ToString() + ". ";
-                foreach (var item in tab)
-                    textTemp += item.ToString();
-                textTemp += "\n";
+                text.Text += count.ToString() + ". ";
+                foreach (var item in buttons)
+                    text.Text += item.Content.ToString();
+                text.Text += " " + SortPercent() + "%" + "\n";
+                scroll.ScrollToBottom();
+                progress.Value = SortPercent();
             }
-
-            for(int i = 0; i < buttons.Length; i++)
-                buttons[i].Content = tab[i].ToString();
-
-            text.Text = textTemp;
-
-            MessageBox.Show("Liczba losowań: " + count);
         }
 
         private bool IsSort()
         {
             bool sort = true;
 
-            for(int i = 1; i < buttons.Length; i++)
-                if (int.Parse(tab[i].ToString())
-                    < int.Parse(tab[i - 1].ToString()))
+            for(int i = 1; i < buttons.Count; i++)
+                if (int.Parse(buttons[i].Content.ToString())
+                    < int.Parse(buttons[i - 1].Content.ToString()))
                 {
                     sort = false;
                     break;
                 }
 
             return sort;
+        }
+
+        private int SortPercent()
+        {
+            int maxval = 0;
+            float sum = 0, avg;
+            
+            foreach (var item in buttons)
+                if (int.Parse(item.Content.ToString()) >= maxval)
+                {
+                    sum++;
+                    maxval = int.Parse(item.Content.ToString());
+                }
+            
+            avg = (sum / buttons.Count) * 100;
+
+            return (int)avg;
         }
     }
 }
